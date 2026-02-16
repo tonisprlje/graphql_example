254aef3545d8327959b75f1770525abf55a0cee4
Toni Sprlje <toni.stringi.sprlje@gmail.com>
2026-02-16 21:38:13 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,56 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 
 const client = new ApolloClient({
-  uri: 'https://spacex-production.up.railway.app/',
+  uri: "https://spacex-production.up.railway.app/",
 });
 
-function App(){
-  const [data, setData] = useState()
+function App() {
+  const [data, setData] = useState();
 
   async function getData() {
     const data = await client
-    .query({
-      query: gql`
-      query($limit: Int) {
-          launchesPast(limit: $limit) {
+      .query({
+        query: gql`
+          query ($limit: Int) {
+            rockets(limit: $limit) {
               id
-              mission_name
-              rocket {
-              rocket_name
+              name
+              mass {
+                kg
               }
-              upcoming
-              launch_date_unix
+              company
+              cost_per_launch
+              height {
+                meters
+              }
+              first_flight
+              type
+              wikipedia
+            }
           }
-      }
-  `,
-  fetchPolicy: "network-only",
-    variables: {
-        limit: 10,
-    }
-    })
-    .then((result) => result)
+        `,
+        fetchPolicy: "network-only",
+        variables: {
+          limit: 10,
+        },
+      })
+      .then((result) => result);
 
-    return data
+    return data;
   }
 
-  async function handleClick(){
-      const res = await getData()
-      setData(res.data.launchesPast)
+  async function handleClick() {
+    const res = await getData();
+    setData(res.data.rockets);
   }
 
-  return(<>
-    <div>
-      <button onClick={handleClick}>Get data</button>
-      <ul>
-        {data?.length && data.map(i=>{return(
-          <li key={i.id}>{i.mission_name}</li>
-          )})}
-      </ul>
-    </div>
-  </>)
-};
+  return (
+    <>
+      <div>
+        <button onClick={handleClick}>Get data</button>
+
+        <ul>
+          {data?.length &&
+            data.map((i) => {
+              return (
+                <>
+                  <li key={i.id}>{i.first_flight}</li>
+                  <li key={i.id}>{i.type}</li>
+                  <li key={i.id}>{i.wikipedia}</li>
+                  <br></br>
+                  <br></br>
+                </>
+              );
+            })}
+        </ul>
+      </div>
+    </>
+  );
+}
 
 export default App;
